5dc958c76b676517c63eb850a83fb918e928fafe
zart1964 <55561825+zart1964@users.noreply.github.com>
2023-12-16 18:02:08 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,12 @@
-let item = 0
 I2C_LCD1602.LcdInit(39)
-I2C_LCD1602.ShowString("Hello", 0, 0)
+I2C_LCD1602.BacklightOff()
+basic.pause(500)
+I2C_LCD1602.BacklightOn()
+I2C_LCD1602.ShowString("OSOYOO", 0, 0)
+I2C_LCD1602.ShowString("\"Hello\"", 3, 1)
+I2C_LCD1602.off()
+basic.pause(500)
+I2C_LCD1602.on()
 basic.forever(function () {
-    item += 1
-    I2C_LCD1602.ShowNumber(item, 0, 1)
-    basic.pause(1000)
+	
 })
